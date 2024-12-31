80ec1f87cb09bef7abb29b55a9c3adb81d3b42f2
palpitate013 <181289683+palpitate013@users.noreply.github.com>
2024-12-31 14:16:35 -0500

    Added for loop to repeat the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let originalString = string
+    for (let i = 1; i < num; i++){
+        string = string + originalString
+    }
+    return string
 };
+repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
